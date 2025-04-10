2b21a521eb457d2e87a06fbc5b13913d3878b271
Kevin <kevin22.073@gmail.com>
2025-04-10 10:40:41 +0200

    adjusted import

--- model/train_model.py
+++ model/train_model.py
@@ -1,7 +1,7 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from modules.data_loader import load_and_clean_data
+from modules.utils.data_loader import load_and_clean_data
 import xgboost as xgb
 import pandas as pd
 import joblib
